f1f3c0303cb8b697fe4a873eb0823c2d0f029354
BryanRuan <89418563+BryanRuan@users.noreply.github.com>
2022-11-20 23:34:14 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
  function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15.5,
-    center: { lat: 41.877359882395616, lng: -87.61193120547523},
+    center: { lat: 41.84245809586425, lng: -87.64776056933353},
   });
   var marker = new google.maps.Marker({
-    position: { lat: 41.883949366404, lng: -87.61204036541213},
+    position: { lat: 41.84245809586425, lng: -87.64776056933353},
     map: map,
   });
   var infowindow = new google.maps.InfoWindow({
-    content:"This is the location of the Yacht Club that I sail out of. The red circle is the area where we usually sail during practice."
+    content:"This is the location is a park"
   });
   infowindow.open(map, marker);
   }
